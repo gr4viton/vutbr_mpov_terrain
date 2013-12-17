22c0823e854ca49410ff9eb11dd2e38cf1a4ef74
gr4viton <lordmutty@gmail.com>
2013-12-17 23:09:47 +0100

    fprinting nicely

--- _functions/GET_features.m
+++ _functions/GET_features.m
@@ -1,16 +1,18 @@
-function [ ftrList ] = GET_features( segim, labels, modes, regsize, grad, conf )
+function [ ftrList ] = GET_features( segIm, labels, modes, regsize, grad, conf )
 %GET_FEATURES gets feature-lists for individual segments
 %   ...
 
 %% anonymous functions for index parameters
 % featureList([ index ]) - get index of segment from labels(y,x)
 fi = @(y,x) labels(y,x)+1;
-iSegm_max = max(labels(:)) + 1;
+
+iSegm_max =  max(labels(:));
+% iSegm_sum = iSegm_max - 1;
 
 disp(['    - Number of segments = ', num2str(iSegm_max+1)]);
 
 % predefining the size of featureList
-ftrList(iSegm_max+1).areaSumAbs = 0;
+ftrList(iSegm_max).segmentIndex = iSegm_max;
 
 % [ featureList(index) ] - get featureList that corresponds to labels(y,x)
 % fL = @(y,x) featureList(fi(y,x));
@@ -21,30 +23,32 @@ ftrList(iSegm_max+1).areaSumAbs = 0;
 % for i = 1:fi_max
 %     disp([ 'featureList(',num2str(i),')=',num2str( ftrList(i).areaSumAbs ) ])
 % end
-% percent of image
+    imageArea = size(segIm,1)*size(segIm,2);
+    
         
 %% first loop through - for inicializing zero values and count the one-loop countable features
 for iSegm = 1:iSegm_max
 % stats for segments as for a group of areas
     
 % ____________________________________________________
-% only inicialized
-    ftrList(iSegm).areaSumAbs2 = 0; % sum of all the pixels
-    ftrList(iSegm).areaSumRelIm = 0; % area sum relatively to whole image
+% inicialization
+    ftrList(iSegm).segmentIndex = iSegm; % segment index number
+    ftrList(iSegm).areaSumAbs = 0; % sum of all the pixels
+    ftrList(iSegm).areaSumRel = 0; % area sum relatively to the others - max area = 100%
+    ftrList(iSegm).areaSumRelIm = 0; % area sum relatively to whole image = x%
+    ftrList(iSegm).eulerNum8 = 0; % eulers number of a segment (genus) [8-neighbor]
     
 %     ftrList(i).circumfrnc = 0; % sum of circumferences of individual areas of a segment
 
 % ____________________________________________________
-% counted here
-    
+% computation
     xlabels = labels; 
     xlabels(xlabels~=iSegm) = 0; % get individual segment of index i - 
     % on the pixels of segment in the picture is the index of segment
     
-    % eulers number of a segment (genus) [8-neighbor]
-    ftrList(iSegm).eulerNum8 = bweuler( uint16(xlabels) ,8);
-    % sum of all the pixels
     ftrList(iSegm).areaSumAbs = sum(xlabels(:)) ./ iSegm;
+    ftrList(iSegm).areaSumRelIm =  ftrList(iSegm).areaSumAbs / imageArea * 100;
+    ftrList(iSegm).eulerNum8 = bweuler( uint16(xlabels) ,8);
 
 % ____________________________________________________
 % others
@@ -57,6 +61,8 @@ for iSegm = 1:iSegm_max
 
 % mean color RGB - of default image or segmented color => is it the same?
 % stats for whole image - from meanshift & kmeans?
+% elipsoidnost jednotlivıch? -> natoèení a tak
+
 %%  asi ne
 % vıstøednost
 % podlouhlost - pomìr stran obdelníka
@@ -66,23 +72,14 @@ for iSegm = 1:iSegm_max
 % boxHeight
 end
 
-
-for y = 1:size(labels,1)
-    for x = 1:size(labels,2)
-        ftrList(fi(y,x)).areaSumAbs2 = ftrList(fi(y,x)).areaSumAbs2 + 1;
-    end
-end
-
-
 %% second loop through
-[areaSumAbs_minVal, areaSumAbs_minIndex]  = min([ ftrList(iSegm).areaSumAbs ]);
+% [areaSumAbs_maxVal, areaSumAbs_maxIndex]  = max([ ftrList(iSegm).areaSumAbs ]);
+areaSumAbs_maxVal  = max([ ftrList.areaSumAbs ]);
 
 for iSegm = 1:iSegm_max
     % area sum relatively to the others - min area = 1
-    ftrList(iSegm).areaSumRel = ftrList(iSegm).areaSumAbs / areaSumAbs_minVal; 
+    ftrList(iSegm).areaSumRel = ftrList(iSegm).areaSumAbs / areaSumAbs_maxVal; 
 end
-
-
 
 %% euler number testing
 % figure(51);
@@ -106,14 +103,5 @@ end
 % bar(all_areaSumAbs_values);
 
 disp(ftrList);
-
-
-% elipsoidnost jednotlivıch? -> natoèení a tak
-% 
-
-
-% stats.percentage
-
-
 end
 
--- _functions/WRITE_statistics.m
+++ _functions/WRITE_statistics.m
@@ -6,39 +6,76 @@ if writeStatsPath~=0
 disp('> Writing feature lists data to statistics file');
 
 % string constants
-delim = ';'; % delimiter of columns
 statsPrefix = 'stats_';
 statsExtension = '.csv';
+delimVal = ';\t\t'; % delimiter of columns
+delimHead = ';\t'; % delimiter of columns
+newLine = '\n';
+% first line of stats-file
+firstLine = ['Feature list for individual segments of image: "',imFileName,'". Row = segment. Delimiter = "',delimVal,'". Columns'' headers:\n'];
+valueCell_strFormat = ['%s',delimVal];
+headerCell_strFormat = ['%s',delimHead];
 
-%% write all stats to string
-% header
-str = ['Feature list for individual segments of image: "',imFileName,'". Row = segment. Delimiter = "',delim,'". Columns'' headers:\n'];
-str = [str, 'Segment index; '];
 
-% automatical insertion of headers from [stats]-structure variable-names
-
-fnam = 'foo.mat';
-save(fnam,'-struct','ftrList');
-
-iSegm_max = size(ftrList,2); % ftrList num of segments
-for i_segm = 1:iSegm_max
-
-%     str = [str,'Segment ',int2str(i),':','    ',sprintf('%.2f', percentage(i)),'%%\n'];
-
-    fi_max = 10; % ftrList num of variables
-    % loop for individual features of ftrList
-    for fi = 1:fi_max
-        val = 0;
-        str = [str, num2str(val), delim];
-    end
-end
-
+% %% write all stats to string
+% 
+% % automatical insertion of headers from [stats]-structure variable-names
+% 
+% iSegm_max = size(ftrList,2); % ftrList num of segments
+% for i_segm = 1:iSegm_max
+%     fi_max = 10; % ftrList num of variables
+%     % loop for individual features of ftrList
+%     for fi = 1:fi_max
+%         val = 0;
+%         str = [str, num2str(val), delim];
+%     end
+% end
+% 
 %% name of stats-file
 statsFileName = strcat(writeStatsPath,statsPrefix,imFileName,statsExtension);
+% 
+% %% open, write & close stats-file 
+% fid = fopen(statsFileName,'w');
+% fprintf(fid, str);
+% fclose(fid);
 
-%% open, write & close stats-file 
-fid = fopen(statsFileName,'w');
-fprintf(fid, str);
+%%  Extract field data
+
+% fields = repmat(fieldnames(ftrList), numel(ftrList), 1);
+featureNames = fieldnames(ftrList)';
+values = struct2cell(ftrList);
+
+%% Convert all numerical values to strings
+idx = cellfun(@isnumeric, values); 
+values(idx) = cellfun(@num2str, values(idx), 'UniformOutput', 0);
+
+%% Combine field names and values in the same array
+% C = {fields{:}; values{:}};
+
+features_sum = numel(featureNames);
+segm_sum = size(values, 3);
+
+%% Write fields to CSV file
+fid = fopen(statsFileName, 'wt');
+fprintf(fid, firstLine);
+
+% columns' headers
+head_strFormat = repmat( headerCell_strFormat, 1, features_sum);
+fprintf(fid, [head_strFormat, newLine], featureNames{:});
+% fprintf(fid, newLine);
+
+% one row
+oneRow_strFormat = repmat(valueCell_strFormat, 1, features_sum)
+% allValues_strFormat = repmat( [oneRow_strFormat, newLine], size(values, 3), 1)
+%     fprintf(fid, allValues_strFormat, values{:});
+% for i=1:17
+% disp( ['values[',num2str(i),']=',values{i}]);
+% end
+for fi = 0:segm_sum-1
+    pointer = fi*features_sum;
+    fprintf(fid, [oneRow_strFormat, newLine], ...
+        values{ pointer+1 : pointer + features_sum });
+end
 fclose(fid);
 
 disp(strcat('  * Statistics of segmented image "',imFileName, '"', ...
